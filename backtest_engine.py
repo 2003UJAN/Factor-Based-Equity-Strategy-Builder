8c54fd3cc93d4d7de77df2a17bd8fae162236259
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-04-27 05:38:22 +0530

    Update backtest_engine.py

--- backtest_engine.py
+++ backtest_engine.py
@@ -1,15 +1,12 @@
 # backtest_engine.py
 import backtrader as bt
 import matplotlib
-matplotlib.use('Agg')  # Important for Streamlit!
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import io
 
 class FactorBasedStrategy(bt.Strategy):
-    params = dict(
-        factor='PERatio',
-        ascending=True,
-    )
+    params = dict(factor='PERatio', ascending=True)
 
     def __init__(self):
         pass
@@ -20,12 +17,16 @@ class FactorBasedStrategy(bt.Strategy):
         elif self.data.close[0] < self.data.close[-1]:
             self.sell()
 
-def run_backtest(data, factor="PERatio", ascending=True):
+def run_backtest(data, factor="PERatio", ascending=True, benchmark_data=None):
     cerebro = bt.Cerebro()
     cerebro.addstrategy(FactorBasedStrategy, factor=factor, ascending=ascending)
 
     datafeed = bt.feeds.PandasData(dataname=data)
     cerebro.adddata(datafeed)
+
+    if benchmark_data is not None:
+        benchmark_feed = bt.feeds.PandasData(dataname=benchmark_data)
+        cerebro.adddata(benchmark_feed)
 
     cerebro.broker.set_cash(100000.0)
     cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name="sharpe")
