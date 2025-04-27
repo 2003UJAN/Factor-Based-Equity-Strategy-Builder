18ca84481d24379915b1f2bbc03bd6635f8c9954
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-04-27 05:33:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 # utils.py
 import pandas as pd
+import yfinance as yf
 
-def generate_dummy_price_data(symbol, start='2020-01-01', end='2023-01-01'):
-    dates = pd.date_range(start=start, end=end)
-    prices = pd.Series(100 + (pd.Series(range(len(dates))) * 0.5).values, index=dates)
-    df = pd.DataFrame({'close': prices})
-    df.index.name = 'date'
-    return df
+def fetch_price_data(symbol, start='2020-01-01', end='2023-01-01'):
+    data = yf.download(symbol, start=start, end=end)
+    if data.empty:
+        raise ValueError("No data fetched. Check symbol and date range.")
+    data = data[['Close']]
+    data.rename(columns={"Close": "close"}, inplace=True)
+    return data
