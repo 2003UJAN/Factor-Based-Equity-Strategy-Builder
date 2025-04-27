452cc07e0a22e90283a6d6d84b80be8b4c519fd9
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-04-27 05:37:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,3 +9,11 @@ def fetch_price_data(symbol, start='2020-01-01', end='2023-01-01'):
     data = data[['Close']]
     data.rename(columns={"Close": "close"}, inplace=True)
     return data
+
+def load_uploaded_data(uploaded_file):
+    data = pd.read_csv(uploaded_file)
+    data.columns = [col.lower() for col in data.columns]
+    if 'close' not in data.columns:
+        raise ValueError("CSV must contain a 'close' column.")
+    data.index = pd.to_datetime(data['date'], errors='coerce')
+    return data
